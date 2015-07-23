2047f50a3a1056512c7ce0abba0310c297a42f2b
Omar Garza <omarg@Omars-MacBook-Pro.local>
2015-07-22 23:13:39 -0500

    Delete Chrome plugin

--- chrome_app/app/background.js
+++
@@ -1,5 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log('Parsing');
-  chrome.tabs.executeScript(null, {file: "app/bower_components/zepto/zepto.min.js"});
-  chrome.tabs.executeScript(null, {file: "app/ScheduleParserChrome.js"});
-});
